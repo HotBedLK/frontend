ca008cc9023202e30f73d0f6d60a06c1df58011a
tgsnimesh <tgsnimesh@gmail.com>
2025-10-13 17:06:31 +0530

    Initialize basic component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className='text-4xl font-bold'>HotBed LK</h1>
-  )
+export default function App() {
+  return <div className="min-h-screen bg-white text-gray-900"></div>;
 }
-
-export default App
