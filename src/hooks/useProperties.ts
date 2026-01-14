8d37ea22b56bd43eb5f0bd46b1ed4af2a88445ad
tgsnimesh <tgsnimesh@gmail.com>
2026-01-14 20:12:03 +0530

    [Testing] : testing data change

--- src/hooks/useProperties.ts
+++ src/hooks/useProperties.ts
@@ -13,45 +13,45 @@ export const useProperties = () => {
         setLoading(true);
         const data = await getProperties();
         // testing data only **************
-        // [
-        //   {
-        //     id: "nawala-home",
-        //     title: "Annex",
-        //     status: "Available",
-        //     location: "Nawala, Nugegoda, Sri Lanka",
-        //     price: "Rs.1500000.00",
-        //     imageUrl: "/images/banners/landing-p-hero-01.png",
-        //   },
-        //   {
-        //     id: "kandy-home1",
-        //     title: "Home",
-        //     status: " Not Available",
-        //     location: "Kandy, Sri Lanka",
-        //     price: "Rs.950000.00",
-        //     imageUrl: "/images/banners/landing-p-hero-01.png",
-        //   },
-        //   {
-        //     id: "galle-home11",
-        //     title: "Apartment",
-        //     status: "Available",
-        //     location: "Galle, Sri Lanka",
-        //     price: "Rs.1200000.00",
-        //     imageUrl: "/images/banners/landing-p-hero-01.png",
-        //   },
-        //   {
-        //     id: "galle-home2",
-        //     title: "Home",
-        //     status: "About to Available",
-        //     location: "Galle, Sri Lanka",
-        //     price: "Rs.1200000.00",
-        //     imageUrl: "/images/banners/landing-p-hero-01.png",
-        //   },
-        // ];
-        setProperties(data);
+        setProperties([
+          {
+            id: "nawala-home",
+            title: "Annex",
+            status: "Available",
+            location: "Nawala, Nugegoda, Sri Lanka",
+            price: "Rs.1500000.00",
+            imageUrl: "/images/banners/landing-p-hero-01.png",
+          },
+          {
+            id: "kandy-home1",
+            title: "Home",
+            status: " Not Available",
+            location: "Kandy, Sri Lanka",
+            price: "Rs.950000.00",
+            imageUrl: "/images/banners/landing-p-hero-01.png",
+          },
+          {
+            id: "galle-home11",
+            title: "Apartment",
+            status: "Available",
+            location: "Galle, Sri Lanka",
+            price: "Rs.1200000.00",
+            imageUrl: "/images/banners/landing-p-hero-01.png",
+          },
+          {
+            id: "galle-home2",
+            title: "Home",
+            status: "About to Available",
+            location: "Galle, Sri Lanka",
+            price: "Rs.1200000.00",
+            imageUrl: "/images/banners/landing-p-hero-01.png",
+          },
+        ]);
+        // setProperties(data);
       } catch (err) {
         setError("Failed to load properties");
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     };
 
